4d11e1a1842779d44f8f93c544f3710927fbcc08
Thomas THIMOTHEE <thomas@oxynum.fr>
2016-09-06 18:02:20 +0200

    Update csv_utils.rb

--- lib/csv_utils.rb
+++ lib/csv_utils.rb
@@ -3,8 +3,8 @@ require 'csv'
 module CSVUtils
 
   def self.csv_to_tuple(file)
-    csv_array = CSV.new(file, headers:true).to_a
-    csv_array.to_a.map {|row| row.to_hash }
+    csv_array = CSV.new(file, headers:true)
+    csv_array.to_a.map &:to_hash
   end
 
 end
